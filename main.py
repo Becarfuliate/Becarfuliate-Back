0eff9d3480c995843a9ac63744d5af138d08d4eb
JYornet <juan.yornet@mi.unc.edu.ar>
2022-10-16 07:12:02 -0300

    se agregan CORS

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from dependency import *
 from routers.match_controller import match_end_points
 from db.database import gen_map
 from models import entities
 
+
 app = FastAPI()
+# Agregando cors urls
+origins = ["http://localhost:3000", "localhost:3000"]
+# Agregando middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 gen_map()
 app.include_router(match_end_points)
