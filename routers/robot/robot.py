30410810fad7916e0e2a6e24364633f645f795c7
JYornet <juan.yornet@mi.unc.edu.ar>
2022-11-02 15:06:31 -0300

    Forma de grados respecto al plano, funciona pero anti intuitivo

--- routers/robot/robot.py
+++ routers/robot/robot.py
@@ -48,7 +48,7 @@ def in_range(base_polar_cord, robot_polar_cord, point_a, point_b):
     # Si los grados están entre el punto a y b
     if robot_polar_cord[1] in range(point_b[1], point_a[1]):
         # Si la distancia del robot es menor a la del rango del radar
-        if robot_polar_cord[0] <= base_polar_cord[0]:
+        if abs(robot_polar_cord[0] - base_polar_cord[0]) <= point_a[0]:
             is_inside = True
 
     return is_inside
@@ -59,26 +59,34 @@ def check_walls(A, B):
     Chequea que en la dirección y profundidad del scaner haya una pared,
     y retorna su coordenada.
     """
+    x_1 = A[0]
+    y_1 = A[1]
+
+    x_2 = B[0]
+    y_2 = B[1]
+
     coordinates = []
     if A[0] <= 0:
-        coordinates.append((0, A[1]))
+        x_1 = 0
     elif A[0] >= 999:
-        coordinates.append((999, A[1]))
-    # Fix this, two cases add.
+        x_1 = 999
     if B[0] <= 0:
-        coordinates.append((0, B[1]))
+        x_2 = 0
     elif B[0] >= 999:
-        coordinates.append((999, B[1]))
+        x_2 = 999
     if A[1] <= 0:
-        coordinates.append((A[0], 0))
+        y_1 = 0
     elif A[1] >= 999:
-        coordinates.append((A[0], 999))
-    # Fix this, two cases add.
+        y_1 = 999
     if B[1] <= 0:
-        coordinates.append((B[0], 0))
+        y_2 = 0
     elif B[1] >= 999:
-        coordinates.append((B[0], 999))
+        y_2 = 999
 
+    if (x_1 == (0 or 999)) or (y_1 == (0 or 999)):
+        coordinates.append((x_1, y_1))
+    if (x_2 == (0 or 999)) or (y_2 == (0 or 999)):
+        coordinates.append((x_2, y_2))
     return coordinates
 
 
@@ -111,6 +119,20 @@ class robot:
             direction = -direction
         elif direction >= 360:
             direction %= 360
+
+        # scanner_degree = direction
+
+        # if self.direction <= 180 and self.direction >= 0:
+        #     if scanner_degree > 0 and scanner_degree < 180:
+        #         scanner_degree -= 45
+        #     else:
+        #         scanner_degree += 45
+        # if self.direction > 180 and self.direction <= 360:
+        #     if scanner_degree > 0 and scanner_degree < 180:
+        #         scanner_degree -= 45
+        #     else:
+        #         scanner_degree += 45
+
         self.direction_scanner = direction
         self.resolution_in_degrees = resolution_in_degrees
         self.scanner_range = amplitude_to_depth(resolution_in_degrees)
@@ -127,12 +149,14 @@ class robot:
     # Setter
     def scan(self, *robots_position):
         s_polar_cord = cart_to_polar(self.position[0], self.position[1])
+        # s_polar_cord = (700, 44°) in cart --> (500,490)
 
         for r_position in robots_position:
 
             # Para cada robot tomo su cordenada polar
             # Convert coordinates in polar coordinates
             r_polar_cord = cart_to_polar(r_position[0], r_position[1])
+            # (450, 43°)
 
             # a = profundidad del escaner + la dirección del robot + apertura
             # a = (500, 45° + 10°)
@@ -145,10 +169,26 @@ class robot:
                 self.direction_scanner - self.resolution_in_degrees,
             )
             # Ojo coor_walls puede ser una lista
-            coor_walls = check_walls(polar_to_cart(point_a), polar_to_cart(point_b))
-            self.scanned_list.append(coor_walls)
+            # coor_walls = check_walls(polar_to_cart(point_a), polar_to_cart(point_b))
+            # self.scanned_list.append(coor_walls)
             # Tengo que chequear que la coordenada polar esté
             # en la misma dirección y a una distancia <= rango del escáner.
             if in_range(s_polar_cord, r_polar_cord, point_a, point_b):
                 result = polar_to_cart(r_polar_cord[0], r_polar_cord[1])
                 self.scanned_list.append(result)
+
+
+# r1 = robot((500, 490), 90)
+# r2 = robot((500, 500), 90)
+# r1.point_scanner(90, 10)
+# r1.scan(r2.position)
+# escaneado = r1.scanned()
+# print(escaneado)
+
+
+r1 = robot((0, 500), 90)
+r2 = robot((500, 500), 90)
+r1.point_scanner(42, 4)
+r1.scan(r2.position)
+escaneado = r1.scanned()
+print(escaneado)
